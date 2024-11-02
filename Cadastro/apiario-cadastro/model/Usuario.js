e636722f501c98a7a0fc03363a96e6b64bc753e9
Rafael Barros <rgracilianobarros@gmail.com>
2024-11-02 18:42:42 -0300

    crud modulo usuario finalizado

--- Cadastro/apiario-cadastro/model/Usuario.js
+++ Cadastro/apiario-cadastro/model/Usuario.js
@@ -1,4 +1,6 @@
+const { query } = require('express');
 const connection = require('../config/db')
+const crypto = require('crypto')
 
 class Usuario {
 
@@ -11,9 +13,12 @@ class Usuario {
     //CRUD - cadastro
     static async criaUsuario(dados){
         const {nome, email, senha} = dados;
+
+        const hash = crypto.createHash('sha256').update(senha).digest('hex');
+
         return new Promise((resolve, reject)=> {
-            const query = 'insert into usuario(nome, email, senha) values(?, ?, ?)'
-            connection.query(query, [nome, email, senha], (error, results)=>{
+            const query = 'insert into Usuario(nome, email, senha) values(?, ?, ?)'
+            connection.query(query, [nome, email, hash], (error, results)=>{
                 if (error){
                     return reject(error)
                 }
@@ -21,9 +26,61 @@ class Usuario {
             })
         })
     }
+
     //CRUD - exclusao
+    static async excluirUsuario(idUsuario) {
+        const id = idUsuario
+        return new  Promise ((resolve,reject) => {
+            const query = 'DELETE FROM Usuario WHERE id = ?';
+            connection.query(query,[id], (error,results) => {
+                if (error) {
+                    return reject(error);
+                }
+                resolve(results.effectedRows);
+            })
+
+        })
+    }
+
+
     //CRUD - atualizacao
+    static async atualizarUsuario(dados) {
+
+        const {valor, id} = dados
+
+        console.log(dados)
+
+        return new Promise ((resolve, reject) => {
+            const query = 'update Usuario set email = ? where id = ?'
+            connection.query(query, [valor, id], (error,results) => {
+                if (error) {
+                    return reject(error);
+                }
+                resolve(results.effectedRows);
+            })
+        }) 
+    }
+
     //CRUD - leitura
+    static async mostraUsuario(dados){
+        const id = dados
+
+        return new Promise ((resolve, reject)=>{
+            const query = 'select * from Usuario where id = ?'
+            connection.query(query, id, (error, results)=>{
+                if(error){
+                    return reject(error)
+                }
+
+                if(results.length > 0){
+                    resolve(results[0])
+                } else {
+                    resolve(null)
+                }
+                
+            })
+        })
+    }
 }
 
 module.exports = Usuario
--- Cadastro/apiario-cadastro/testUsuario.js
+++ Cadastro/apiario-cadastro/testUsuario.js
@@ -1,11 +1,13 @@
 const Usuario = require('./model/Usuario'); 
 
+
+
 // Função de teste para criar um novo usuário
 async function testarCriarUsuario() {
   try {
     const dadosUsuario = {
-      nome: 'João da Silva',
-      email: 'joao.silva@example.com',
+      nome: 'Rafael Barros',
+      email: 'rafaelbarros@gmail',
       senha: 'senha123'
     };
 
@@ -18,7 +20,54 @@ async function testarCriarUsuario() {
   }
 }
 
-// Executa a função de teste
-testarCriarUsuario();
+//testarCriarUsuario();
 
+
+
+async function deleteUsuario() {
+  try{
+    const id = 3;
+    const usuarioId = await Usuario.excluirUsuario(id);
+    console.log(`Usuário excluido com ID: ${usuarioId}`);
+
+  }
+  catch (error) {
+    console.error('Erro ao criar usuário:', error);
+  } finally {
+    process.exit(); // Encerra o processo após o teste
+  }
+}
+
+//deleteUsuario()
+
+async function updateUsuario() {
+  try {
+    const dadosUsuario = {
+      valor: 'kleiton@gmail',
+      id: 1
+    }
+    
+    const usuarioId = await Usuario.atualizarUsuario(dadosUsuario)
+    console.log(`Usuário atualizado com ID: ${usuarioId}`)
+
+  }
+  catch (error) {
+    console.error('Erro ao criar usuário:', error)
+  } finally {
+    process.exit(); // Encerra o processo após o teste
+  }
+
+  
+}
+
+//updateUsuario()
+
+async function exibeUsuario() {
+  const id = 1
+
+  const res = await Usuario.mostraUsuario(id)
+  console.log(res)
+}
+
+//exibeUsuario()
 
