7a5d2e47935a3260dd1f60c484580ee0fe7660d2
kleiton <kleytonferreira9@gmail.com>
2024-11-02 12:22:45 -0300

    conectando com o Mysql

--- config/db.js
+++ config/db.js
@@ -1,16 +1,24 @@
 
+const dotenv = require('dotenv');
 const mysql = require('mysql2');
 
+dotenv.config();
+
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'seu_usuario',
-    password: 'sua_senha',
-    database: 'apiario'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
+
 
 connection.connect((err) => {
     if (err) throw err;
     console.log('Conectado ao banco de dados MySQL!');
 });
+
+
+
 
 module.exports = connection;
